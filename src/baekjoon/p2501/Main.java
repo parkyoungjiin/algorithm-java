beb2941b8de1514d8ad15a8e7175dfdf5a1a4b45
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-10-22 23:47:42 +0900

    2501, 3460

--- src/baekjoon/p2501/Main.java
+++ src/baekjoon/p2501/Main.java
@@ -1,2 +1,34 @@
-package baekjoon.p2501;public class Main {
+package baekjoon.p2501;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int k = sc.nextInt();
+        String[] input = sc.nextLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        // n의 약수 구하기
+        for (int i = 1; i <= n; i++){
+            if ((n % i) == 0){
+                arr.add(i);
+            }
+        }
+
+        // k개보다 약수가 적은경우 0
+        if (arr.size() < k){
+            System.out.println(0);
+        }else{
+            System.out.println(arr.get(k-1));
+        }
+
+
+    }
 }
--- src/baekjoon/p3460/Main.java
+++ src/baekjoon/p3460/Main.java
@@ -1,2 +1,26 @@
-package baekjoon.p3460;public class Main {
+package baekjoon.p3460;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        sc.nextLine();
+
+        for (int t=0; t<tc; t++){
+            int n = sc.nextInt();
+
+            //2진수 변환
+            String bs = Integer.toBinaryString(n);
+            char[] ans = bs.toCharArray();
+
+            //1의 위치 공백으로 한 줄에 출력
+            for (int i = 0; i < ans.length; i++){
+                if (ans[i] == '1'){
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
 }
