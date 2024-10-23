6fefc8f86f1295d1fbb375ab938a00ba1b1db340
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-10-23 12:21:10 +0900

    10818 array 사용 풀이

--- src/baekjoon/p10818/Main.java
+++ src/baekjoon/p10818/Main.java
@@ -1,4 +1,5 @@
 package baekjoon.p10818;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,24 +7,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
-        String[] nums = sc.nextLine().split(" ");
+        int [] nums = new int[n];
 
-        //output: 최솟값 최대값
-
-        int minValue, maxValue;
-        minValue = maxValue = Integer.parseInt(nums[0]);
-
-        for (int i = 1 ; i <n; i++){
-            int num = Integer.parseInt(nums[i]);
-
-            if (num < minValue){
-                minValue = num;
-            }else if (num > maxValue){
-                maxValue = num;
-            }
+        for (int i = 0; i < n ; i++){
+            nums[i] = sc.nextInt();
         }
 
-        System.out.println(minValue + " " + maxValue);
+        Arrays.sort(nums);
+
+        //output: 최솟값 최대값
+        System.out.println(nums[0] + " " + nums[n-1]);
+
+
+
+
+
+
 
     }
 }
