3bb6a696c5a8b52050f3f3003b3caf1dc23a56c1
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-10-23 12:15:26 +0900

    10818

--- src/baekjoon/p10818/Main.java
+++ src/baekjoon/p10818/Main.java
@@ -1,2 +1,29 @@
-package baekjoon.p10818;public class Main {
+package baekjoon.p10818;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] nums = sc.nextLine().split(" ");
+
+        //output: 최솟값 최대값
+
+        int minValue, maxValue;
+        minValue = maxValue = Integer.parseInt(nums[0]);
+
+        for (int i = 1 ; i <n; i++){
+            int num = Integer.parseInt(nums[i]);
+
+            if (num < minValue){
+                minValue = num;
+            }else if (num > maxValue){
+                maxValue = num;
+            }
+        }
+
+        System.out.println(minValue + " " + maxValue);
+
+    }
 }
