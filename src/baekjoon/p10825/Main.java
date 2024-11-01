67804385d3fd23c682f28a10dca62ff38aca3de1
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-11-01 10:05:36 +0900

    10825 solve

--- src/baekjoon/p10825/Main.java
+++ src/baekjoon/p10825/Main.java
@@ -6,34 +6,72 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st; //StringTokenizer 인스턴스 정의
         int n = Integer.parseInt(br.readLine());
-        ArrayList<String> studentName = new ArrayList<>();
-        ArrayList<int[]> studentScores = new ArrayList<>();
-        for (int i = 0; i < n ; i++){
-            int[] studentScore = new int[3];
-            String [] students = br.readLine().split(" ");
+        ArrayList<Student> studentList = new ArrayList<>();
 
-            studentScore[0] = Integer.parseInt(students[1]);
-            studentScore[1] = Integer.parseInt(students[2]);
-            studentScore[2] = Integer.parseInt(students[3]);
+        for (int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine()); //StringTokenizer 객체 생성
+            String name = st.nextToken();
+            int korea = Integer.parseInt(st.nextToken());
+            int english = Integer.parseInt(st.nextToken());
+            int math = Integer.parseInt(st.nextToken());
 
-            studentName.add(students[0]); // 학생 이름 add
-            studentScores.add(studentScore); // 학생 성적 add
+//            Student student = new Student(name, korea, english, math);
+            studentList.add(new Student(name, korea, english, math));
+        }//for
 
-        }
+        //Collections.sort
+        Collections.sort(studentList, new Comparator<Student>(){
+            @Override
+            public int compare(Student o1, Student o2){
 
-        for (String s : studentName){
-            System.out.println(s);
-        }
-        for (int[] sc : studentScores){
-            for (int i = 0 ; i < sc.length; i++){
-                System.out.println(sc[i]);
-            }ㅊ
+                //국어 동률
+                if(o1.koreaScore == o2.koreaScore){
+                    //국어=영어
+                    if (o1.engScore == o2.engScore){
+                        //국영수 동률
+                        if (o1.mathScore == o2.mathScore){
+                            //이름 사전순 정렬
+                            return o1.studentName.compareTo(o2.studentName);
+                        }
+                        // 수학 내림
+                        return o1.mathScore - o2.mathScore;
+                    }
+                    //영어 오름
+                    return o1.engScore - o2.engScore;
+                }
+
+
+                //기본값 : 국어 감소
+               return o2.koreaScore - o1.koreaScore;
+            }
+        });
+
+
+    }//main
+
+    static class Student{
+
+        String studentName; //이름
+
+        int koreaScore; //국어
+
+        int engScore; //영어
+
+        int mathScore; //수학
+
+        //생성자
+        public Student(String studentName, int koreaScore, int engScore, int mathScore){
+            this.studentName = studentName;
+            this.koreaScore = koreaScore;
+            this.engScore = engScore;
+            this.mathScore = mathScore;
         }
     }
 
