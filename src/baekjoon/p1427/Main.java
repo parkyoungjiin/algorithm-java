0e9c85fca46242dd43438188dc31d5c28958dbb4
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-11-04 20:33:28 +0900

    1427 solve2

--- src/baekjoon/p1427/Main.java
+++ src/baekjoon/p1427/Main.java
@@ -1,2 +1,32 @@
-package baekjoon.p1427;public class Main {
+package baekjoon.p1427;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        char[] digits = input.toCharArray();
+
+        Arrays.sort(digits); // 오름차순 정렬
+
+        // 오름차순 숫자를 역순으로 출력
+        for(int i = input.length() -1 ; i >= 0 ; i--){
+            System.out.print(digits[i]);
+        }
+
+
+
+
+    }//main
+
 }
