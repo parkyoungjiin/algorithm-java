2e634101227a02a1cf73c2e7830213aa567b6ffe
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-12-05 17:16:28 +0900

    refactor: 1764

--- src/baekjoon/p1764/Main.java
+++ src/baekjoon/p1764/Main.java
@@ -12,43 +12,31 @@ public class Main {
         int n = nums[0];
         int m = nums[1];
 
-        Map<String,Integer> answer = new HashMap<>();
+        HashSet<String> names = new HashSet<>();
 
         for (int i = 0 ; i < n; i++){
             String name = br.readLine();
-            answer.put(name, 0);
-
+            names.add(name);
         }
+
+        ArrayList<String> result = new ArrayList<>();
 
         for (int i = 0 ; i < m; i++){
             String name = br.readLine();
             //name이 존재하는 경우
-            if (answer.containsKey(name)){
-                answer.replace(name, 1);
+            if(names.contains(name)){
+                result.add(name);
             }
 
         }
 
-        // value = 1인 경우 중복되는 학생으로 파악
-        int answer_cnt = 0;
-        ArrayList<String> human = new ArrayList<>();
 
-        // 중복인 학생 이름 출력 + 개수 확인
-        Set<Map.Entry<String, Integer>> ans = answer.entrySet();
+        Collections.sort(result);
 
-        for (Map.Entry<String, Integer> a : ans){
-            if(a.getValue() == 1){
-                answer_cnt++;
-                human.add(a.getKey());
-            }
-        }
+        System.out.println(result.size());
 
-        Collections.sort(human);
-
-        System.out.println(answer_cnt);
-
-        for(String h : human){
-            System.out.println(h);
+        for(String r : result){
+            System.out.println(r);
         }
 
 
