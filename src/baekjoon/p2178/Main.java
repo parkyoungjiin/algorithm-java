6bb7dadcdbcc6485135dd81ebb52741c25347236
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2025-01-16 10:08:19 +0900

    2178 solve

--- src/baekjoon/p2178/Main.java
+++ src/baekjoon/p2178/Main.java
@@ -1,2 +1,88 @@
-package baekjoon.p2178;public class Main {
+package baekjoon.p2178;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+
+public class Main {
+    static int N, M, ans;
+    static int [][] arr, visited;
+
+    static int [] dx = {1, -1, 0, 0};
+    static int [] dy = {0, 0, 1, -1};
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 공백으로 구분된 값 -> StringTokenizer로 사용
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        visited = new int[N][M];
+
+        for(int i=0; i<N; i++){
+            String [] input= br.readLine().split("");
+            //String [] -> int []
+            int [] numbers = new int[input.length];
+
+            for (int k=0; k<input.length; k++){
+                numbers[k] = Integer.parseInt(input[k]);
+            }
+
+            arr[i] = numbers;
+
+        }
+        visited[0][0] = 1;
+        bfs();
+
+
+        ans = visited[N-1][M-1];
+        System.out.println(ans);
+
+
+    }//main
+
+    static void bfs(){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0, 0));
+
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
+
+            for(int i=0; i<4; i++){
+                int nx = dx[i] + x;
+                int ny = dy[i] + y;
+
+                // 범위 확인
+                if (0 <= nx && nx < N && 0 <= ny && ny < M){
+                    //미로 판별
+                    if(arr[nx][ny] == 1 && visited[nx][ny] == 0){
+                        queue.add(new Node(nx,ny));
+                        visited[nx][ny] = visited[x][y] + 1;
+                    }
+                }
+
+
+            }//for
+        }//while
+
+
+
+
+
+    }
+}//Main class
+
+class Node{
+    int x, y;
+    public Node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
--- src/baekjoon/p2606/Main.java
+++ src/baekjoon/p2606/Main.java
@@ -4,47 +4,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Main {
+    static boolean[] visited;
+    static int [][] nodes;
+    static int cnt;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int computers = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
 
-        boolean[] visited = new boolean[computers+1];
+        cnt = 0;
+        visited = new boolean[computers+1];
 
-        int[][] nodes = new int[computers+1][computers+1];
-        //연결 정보
-        for (int i = 0 ; i < n; i++){
-            String[] str = br.readLine().split(" ");
-            int a = Integer.parseInt(str[0]);
-            int b = Integer.parseInt(str[1]);
+        //연결 정보 저장 2차원 배열
+        nodes = new int[computers+1][computers+1];
 
+        //인접 행렬 값 넣기
+        for(int i=0; i<n; i++){
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
             nodes[a][b] = nodes[b][a] = 1;
+
         }
 
-        //1번 감염 시
-        Queue<Integer> queue = new LinkedList<>();
 
-        queue.offer(1);
-        visited[1] = true;
-        int cnt = 0;
+        //1번 감염
+        dfs(1);
+        System.out.println(cnt-1); //1번은 제외
 
-        while(!queue.isEmpty()){
-            int val = queue.poll();
 
-            for(int i = 1; i<=computers; i++){
-                if(nodes[val][i] == 1 && !visited[i]){
-                    visited[i] = true;
-                    queue.offer(i);
-                    cnt++;
-                }
+
+    }
+
+    static void dfs(int index){
+        //방문처리
+        visited[index] = true;
+        cnt++;
+        for(int i = 0 ; i < nodes[index].length; i++){
+            if(nodes[index][i] == 1 && !visited[i]){
+                dfs(i);
+
             }
-
         }
-
-        System.out.println(cnt);
-
-
 
 
     }
