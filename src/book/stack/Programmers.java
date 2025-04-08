438f8d585e1360677d1d885441c06d0e3016a49f
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2025-04-08 15:02:32 +0900

    study java

--- src/book/stack/Programmers.java
+++ src/book/stack/Programmers.java
@@ -3,25 +3,50 @@ package book.stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Programmers {
     public static void main(String[] args) throws IOException {
-        Queue<String> queue = new LinkedList<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int answer = 0;
+        ArrayList<Character> s = new ArrayList<>();
 
-        String s = br.readLine();
-
-        // String -> Queue
-
-        for(int i = 0; i < s.length(); i++){
-            String str = s.substring(i,i+1);
-            queue.add(str);
+        for (int k = 0; k < str.length() ; k++){
+            s.add(str.charAt(k));
         }
 
-        // queue 회전
+        Map<Character, Character> map = new HashMap<>(); // key, value
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
 
+        //
+        for (int i = 0 ; i < s.size() ; i++){
+            Stack<Character> stack = new Stack<>();
+
+            for (int j = 0; j <s.size(); j++){
+                if(stack.isEmpty()){
+                    stack.push(s.get(j));
+                }else if(stack.peek() == map.get(s.get(j))){
+                    stack.pop();
+                }else{
+                    stack.push(s.get(j));
+                }
+            }
+
+            if (stack.size() == 0){
+                answer++;
+            }
+
+            //회전
+            char temp = s.get(0);
+            s.remove(0);
+            s.add(temp);
+        }
+
+
+
+        System.out.println(answer);
     }
 }
--- src/book/stack/Solution.java
+++ src/book/stack/Solution.java
@@ -1,28 +1,40 @@
 package book.stack;
 
+import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
 
 public class Solution {
-    public static String solution(int decimal) {
-        Deque<Integer> stack = new ArrayDeque<Integer>();
-        //1이 될 때까지
-        while (decimal != 1){
-            //나머지는 스택에 저장.
-            stack.push(decimal % 2);
+    public static void main(String[] args) {
+//        solution(10);
+        solution(27);
 
-            //몫은 갱신
-            decimal /= 2;
-        }
-        Iterator<Integer> iterator = stack.iterator();
-
-        while(iterator.hasNext()){
-            Integer val = iterator.next();
-            System.out.println(val);
-        }
-
-        //이터레이터 사용법을 모름
-        return "";
     }
+
+        public static String solution(int decimal) {
+            Deque<Integer> stack = new ArrayDeque<Integer>();
+            //1이 될 때까지
+            while (decimal > 0){
+                //나머지는 스택에 저장.
+                stack.push(decimal % 2);
+
+                //몫은 갱신
+                decimal /= 2;
+            }
+
+            // String 타입 + 연산은 성능이 안좋기에 StringBuilder 사용.
+            StringBuilder sb = new StringBuilder();
+
+            while(!stack.isEmpty()){
+                sb.append(stack.pop());
+            }
+//            Iterator<Integer> iterator = stack.iterator();
+//            while(iterator.hasNext()){
+//                Integer val = iterator.next();
+//                System.out.println(val);
+//            }
+
+            return sb.toString();
+        }
 }
